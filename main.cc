982cb90ec9420f632998b550fd455bfc03e78228
BoomChao <68780403+BoomChao@users.noreply.github.com>
2022-08-05 18:07:02 +0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -4,8 +4,8 @@ int main() {
     std::cout << "This is sha" << std::endl;
     std::cout << "hello wolrd" << std::endl;
 
-    std::cout << "wolrd" << std::endl;
-    std::cout << "world" << std::endl;
+    std::cout << "hello" << std::endl;
+    std::cout << "whello" << std::endl;
 
     return 0;
 }
