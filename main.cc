1dc9f773af468a52632d06d105e522e0dbe93aa4
zhujinchao.0713 <zhujinchao.0713@bytedance.com>
2022-08-05 18:17:20 +0800

    das

--- main.cc
+++ main.cc
@@ -3,11 +3,8 @@
 int main() {
     std::cout << "This is sha" << std::endl;
     std::cout << "hello wolrd" << std::endl;
-
-    std::cout << "dasd" << std::endl;
-    std::cout << "dasda" << std::endl;
-
     
+
     std::cout << "qwe" << std::endl;
     std::cout << "qwe" << std::endl;
     
